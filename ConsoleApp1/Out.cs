1f5d38b2f31854a7c8e1dd7e1e4cfb9d03f8bb4c
Oleg <aleynikov.oleg@gmail.com>
2021-07-13 16:37:13 +0300

    Initial

---
+++ ConsoleApp1/Out.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Out
+    {
+        public void _1()
+        {
+            string arg = "10";
+            var a1 = Int64.TryParse(arg, out var number);
+            long result = number;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var out_ = new Out();
+            out_._1();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
